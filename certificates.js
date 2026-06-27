4957185333aa9643e4c7238d4b4c822299136091
zenkaizenith-maker <zenkaizenith@gmail.com>
2026-06-27 15:59:30 +0500

    certificates.js

--- certificates.js
+++ certificates.js
@@ -6,14 +6,14 @@ const certificates = {
     certificateType: "LanguageCert Level 1",
     module: "ESOL B2",
     examDate: "08 Apr 2025"
-  }
+  },
   "GR526028583QU": {
     firstName: "Qazi Ubaid",
     lastName: "Ullah",
     certificateType: "LanguageCert Level 1",
     module: "ESOL B2",
     examDate: "8 May 2026"
-  }
+  },
 
    "GR526028582WK": {
     firstName: "Waseem ",
@@ -21,40 +21,40 @@ const certificates = {
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Listening, Reading, Writing, Speaking) (Communicator B2)",
     module: "LanguageCert ESOL Β2 International LRWS",
     examDate: "25 Jul 2025"
-  }
+  },
   "GR526028583WK": {
     firstName: "Waseem ",
     lastName: "Khan",
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	11 Jul 2025 "
-}
-  "	GR526034225IU": {
+},
+  "GR526034225IU": {
     firstName: "	IHTISHAM ",
     lastName: "	ULLAH",
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	25 Jul 2025 "
-}
-  "	GR839131818IU": {
+},
+  "GR839131818IU": {
     firstName: "	IHTISHAM ",
     lastName: "	ULLAH",
     certificateType: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "		LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	11 Jul 2025 "
-}
-  "	GR839131818MI": {
+},
+  "GR839131818MI": {
     firstName: "	MUHAMMAD ",
     lastName: "	ISHAQ ",
     certificateType: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "		LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	15 Jul 2025 "
-}
-  "	GR839131819MI": {
+},
+  "GR839131819MI": {
     firstName: "	MUHAMMAD",
     lastName: "	ISHAQ",
    certificateType: "LanguageCert Level 1 Certificate in ESOL International (Listening, Reading, Writing, Speaking) (Communicator B2)",
     module: "LanguageCert ESOL Β2 International LRWS",
     examDate: "	29 Jul 2025 "
-},
+}
 };
