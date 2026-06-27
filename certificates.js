a3bc6be69a2ad3ff0a1ebe3fc87aef9f964a8867
zenkaizenith-maker <zenkaizenith@gmail.com>
2026-06-27 15:55:34 +0500

    certificates.js

--- certificates.js
+++ certificates.js
@@ -6,15 +6,14 @@ const certificates = {
     certificateType: "LanguageCert Level 1",
     module: "ESOL B2",
     examDate: "08 Apr 2025"
-  },
-
+  }
   "GR526028583QU": {
     firstName: "Qazi Ubaid",
     lastName: "Ullah",
     certificateType: "LanguageCert Level 1",
     module: "ESOL B2",
     examDate: "8 May 2026"
-  },
+  }
 
    "GR526028582WK": {
     firstName: "Waseem ",
@@ -22,35 +21,35 @@ const certificates = {
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Listening, Reading, Writing, Speaking) (Communicator B2)",
     module: "LanguageCert ESOL Β2 International LRWS",
     examDate: "25 Jul 2025"
-  },
+  }
   "GR526028583WK": {
     firstName: "Waseem ",
     lastName: "Khan",
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	11 Jul 2025 "
-},
+}
   "	GR526034225IU": {
     firstName: "	IHTISHAM ",
     lastName: "	ULLAH",
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	25 Jul 2025 "
-},
+}
   "	GR839131818IU": {
     firstName: "	IHTISHAM ",
     lastName: "	ULLAH",
     certificateType: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "		LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	11 Jul 2025 "
-},
+}
   "	GR839131818MI": {
     firstName: "	MUHAMMAD ",
     lastName: "	ISHAQ ",
     certificateType: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     module: "		LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
     examDate: "	15 Jul 2025 "
-},
+}
   "	GR839131819MI": {
     firstName: "	MUHAMMAD",
     lastName: "	ISHAQ",
