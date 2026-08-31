69f5914c37d221609457cb93f305cfc12a0a6b96
zenkaizenith-maker <zenkaizenith@gmail.com>
2026-08-30 23:33:28 -0700

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -112,5 +112,19 @@ const certificates = {
     certificateType: "LanguageCert Level 1 Certificate in ESOL International (Listening, Reading, Writing, Speaking) (Communicator B2)",
     module: "LanguageCert ESOL Β2 International LRWS",
     examDate: "	15 Jun 2025 "
-}
+},
+            "GR839131819FK": {
+    firstName: "	FAIZYAB ",
+    lastName: "	KHAN ",
+    certificateType: "	LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
+    module: "		LanguageCert Level 1 Certificate in ESOL International (Speaking) (Communicator B2)",
+    examDate: "	15 Jun 2025 "
+},
+        "GR839131820FK": {
+    firstName: "	FAIZYAB ",
+    lastName: "	KHAN ",
+    certificateType: "LanguageCert Level 1 Certificate in ESOL International (Listening, Reading, Writing, Speaking) (Communicator B2)",
+    module: "LanguageCert ESOL Β2 International LRWS",
+    examDate: "	15 Jun 2025 "
+}                 
 };
